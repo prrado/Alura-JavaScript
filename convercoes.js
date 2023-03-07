5a15e0248ed81466c53d658bc3bf412753182c4d
Caio Ryan <74019374+prrado@users.noreply.github.com>
2023-03-07 15:10:32 -0300

    Add files via upload
    Conversões em JavaScript

---
+++ convercoes.js
@@ -0,0 +1,13 @@
+// Conversão implicita
+
+const numero = 456
+const numeroStirng = Number("456")
+
+//Se usa o objeto Number() para converter uma string em um número
+//Se usa o objeto String() para converter um número em uma String
+//Ambos podem ser usados na declaração da variável ou dentro do console.log como no exemplo abaixo
+
+
+// Conversão explícita
+console.log(numero + Number(numeroStirng))
+
