b3ca2885f2fd7df7b19bef8716348263d56ca461
Caio Ryan <74019374+prrado@users.noreply.github.com>
2023-03-08 15:17:07 -0300

    Add files via upload
    Exemplo de template String

---
+++ templateString.js
@@ -0,0 +1,7 @@
+const nome = "Caio"
+const idade = 2023 - 2003
+const cidade = "São Paulo"
+
+// Template String é um modo de concatenar variáveis de forma mais organizada
+const frase = `Meu nome é ${nome}, tenho ${idade} anos e moro na cidade de ${cidade}` 
+console.log(frase)
